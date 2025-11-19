52cc6170a21bb4663ee89a974d5f7fcd39d1492b
Joni Mäkinen <joni-makinen@live.fi>
2025-11-20 00:12:06 +0200

    Replace ...import SkillAssessor with ...import AssessorAgent

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,4 +1,5 @@
 # Allows importing with: from agents import ...
 from .prompts.skill_assessor_prompts import SYSTEM_PROMPT, USER_PROMPT_TEMPLATE, OUTPUT_SCHEMA_INSTRUCTION
 from .schemas.skill_profile import SkillProfile
-from .skill_assessor import SkillAssessor
+from .skill_assessor import AssessorAgent
+from .searcher import SearcherAgent
