b9d2825fa1e052bdc38f748e7d0115a004b7e485
Joni Mäkinen <joni-makinen@live.fi>
2025-11-20 09:42:48 +0200

    Add 'from .query_builder import build_queries' and 'from .scraper_duunitori import fetch_search_results'

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,4 @@
 # Allows importing with: from utils import ...
 from .normalization import normalize_list
+from .query_builder import build_queries
+from .scraper_duunitori import fetch_search_results
