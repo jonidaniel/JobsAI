60995abfcc17c3c20fd796cbd725ac613fb095dd
Joni Mäkinen <joni-makinen@live.fi>
2025-11-21 21:03:18 +0200

    Add info on deep mode setting

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
 # ---------- SETTINGS ----------
 
 JOB_BOARDS = ["duunitori"]
-DEEP_MODE = True
+DEEP_MODE = True # Set to False if there's no need for full job descriptions when scraping job listings
