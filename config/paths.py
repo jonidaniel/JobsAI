13efc5b552ae33ae8e0066bf732d012f2fbc4e0e
Joni Mäkinen <joni-makinen@live.fi>
2025-11-21 15:43:09 +0200

    Add URLs

--- config/paths.py
+++ config/paths.py
@@ -9,3 +9,8 @@ JOB_LISTINGS_RAW_PATH = "data/job_listings/raw/"
 JOB_LISTINGS_SCORED_PATH = "data/job_listings/scored/"
 
 REPORTS_PATH = "data/reports/"
+
+# ---------- URLS ----------
+
+HOST_URL = "https://duunitori.fi"
+SEARCH_URL_BASE = "https://duunitori.fi/tyopaikat/haku/{query_slug}?sivu={page}"
