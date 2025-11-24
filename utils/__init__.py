e9edd171445b6e6a6f77e28007c60b616dbceb07
Joni Mäkinen <joni-makinen@live.fi>
2025-11-24 13:55:33 +0200

    Comment out everything

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 # Allows importing with: from utils import ...
-from .normalization import normalize_list
-from .query_builder import build_queries
-from .scraper_duunitori import fetch_search_results
+# from .normalization import normalize_list
+# from .normalization import normalize_text
+# from .query_builder import build_queries
+# from .scraper_duunitori import fetch_search_results
