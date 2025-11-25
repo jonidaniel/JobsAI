f1788ef4f86c2ee4874852a7e9438015abcab841
Joni Mäkinen <joni-makinen@live.fi>
2025-11-26 00:48:03 +0200

    Remove file

--- agents/__init__.py
+++
@@ -1,6 +0,0 @@
-# Allows importing with: from agents import ...
-from .profiler import ProfilerAgent
-from .searcher import SearcherAgent
-from .scorer import ScorerAgent
-from .reporter import ReporterAgent
-from .generator import GeneratorAgent
