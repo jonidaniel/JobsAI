186038dec38a1fd982f05fbe2af70ce5af869ad1
Joni Mäkinen <joni-makinen@live.fi>
2025-11-26 00:53:07 +0200

    Remove file

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# Allows importing with: from utils import ...
-# from .normalization import normalize_list
-# from .normalization import normalize_text
-# from .query_builder import build_queries
-# from .scraper_duunitori import fetch_search_results
--- utils/scrapers/jobly.py
+++
@@ -1,7 +0,0 @@
-# ---------- JOBLY SCRAPER ----------
-
-# scrape_jobly
-
-
-def scrape_jobly():
-    return
