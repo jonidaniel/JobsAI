89632b557fccd05b6446d20dc485a73bde023189
Joni Mäkinen <joni-makinen@live.fi>
2025-11-26 13:24:08 +0200

    Rename sections variable as experienceCategories, rename section variable as category

--- frontend/scripts/navigation.js
+++ frontend/scripts/navigation.js
@@ -1,26 +1,27 @@
 // Gather all experience-category-container div's child sections (experience categories) to an array when the DOM content is loaded
 document.addEventListener("DOMContentLoaded", () => {
-  const sections = Array.from(
+  const experienceCategories = Array.from(
     document.querySelectorAll("#experience-categories-container section")
   );
   let currentIndex = 0;
 
   // Toggle a section's (an experience category's) state
   function showSection(index) {
-    sections.forEach((section, i) => {
-      section.classList.toggle("active", i === index);
+    experienceCategories.forEach((category, i) => {
+      category.classList.toggle("active", i === index);
     });
   }
 
   // On click listener for left arrow
   document.getElementById("prevBtn").onclick = () => {
-    currentIndex = currentIndex === 0 ? sections.length - 1 : currentIndex - 1;
+    currentIndex =
+      currentIndex === 0 ? experienceCategories.length - 1 : currentIndex - 1;
     showSection(currentIndex);
   };
 
   // On click listener for right arrow
   document.getElementById("nextBtn").onclick = () => {
-    currentIndex = (currentIndex + 1) % sections.length;
+    currentIndex = (currentIndex + 1) % experienceCategories.length;
     showSection(currentIndex);
   };
 
