65dc13d5c8e6826e9f442b7bcc8256347324281a
Joni Mäkinen <joni-makinen@live.fi>
2025-11-27 21:59:40 +0200

    Add some re-exporting lines

--- src/jobsai/utils/__init__.py
+++ src/jobsai/utils/__init__.py
@@ -1,5 +1,7 @@
 # Allows importing with: from utils import ...
+# from .scrapers.duunitori import scrape_duunitori
+# from .llms import call_llm, extract_json
+# from .normalization import normalize_parsed
 # from .normalization import normalize_list
 # from .normalization import normalize_text
-# from .query_builder import build_queries
-# from .scraper_duunitori import fetch_search_results
+# from .queries import build_queries
