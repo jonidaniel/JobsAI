9e2af3f0d0d6ee4c68d5092f5ccb70ddcf4a8005
Joni Mäkinen <joni-makinen@live.fi>
2025-11-29 01:32:48 +0200

    Make JOB_BOARDS, DEEP_MODE, REPORT_SIZE, LETTER_STYLE, and CONTACT_INFORMATION not constants (job_boards, deep_mode, report_size, letter_style, contact_information)

--- src/jobsai/config/settings.py
+++ src/jobsai/config/settings.py
@@ -1,17 +1,17 @@
 # ---------- SETTINGS ----------
 
 # The job boards to scrape for job listings
-JOB_BOARDS = ["Duunitori"]  # Choose from "Duunitori" and "Jobly"
+job_boards = ["Duunitori"]  # Choose from "Duunitori" and "Jobly"
 # Deep mode allows deeper scraping of job listings
 # If True, the listings are opened individually, i.e., each one is crawled for full job description
-DEEP_MODE = True  # Choose from True or False
+deep_mode = True  # Choose from True or False
 # The size of the job report
-REPORT_SIZE = 1
+report_size = 1
 # The style or tone of the cover letters
-LETTER_STYLE = "professional"  # Choose from "professional", "friendly", or "confident"
+letter_style = "professional"  # Choose from "professional", "friendly", or "confident"
 
 # The candidate's contact information for the cover letters
-CONTACT_INFORMATION = {
+contact_information = {
     "website": "jonimakinen.com",
     "linkedin": "linkedin.com/in/joni-daniel-makinen",
     "github": "github.com/jonidaniel",
