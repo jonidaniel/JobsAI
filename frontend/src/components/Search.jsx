d71609e54032e83acf28eaaf3079e4b4309b9643
Joni Mäkinen <joni-makinen@live.fi>
2025-11-29 22:44:01 +0200

    Modify submit button tailwind utility classes

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -7,14 +7,14 @@ export default function Search() {
     <section id="search">
       <h2>Search</h2>
       <h3 className="text-3xl font-semibold text-white text-center">
-        Fill in questions in each category and we will find jobs relevant to you
+        Answer questions in each category and we will find jobs relevant to you
       </h3>
       <QuestionSets />
       {/* Submit button */}
       <div className="flex justify-center">
         <button
           id="submit-btn"
-          className="mt-6 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow"
+          className="text-3xl mt-6 px-6 py-3 border border-white bg-transparent text-white font-semibold rounded-lg shadow"
         >
           Find Jobs
         </button>
