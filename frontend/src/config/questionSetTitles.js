d519e3388f12a7a17c762c2b4550f699d7bb840a
Joni Mäkinen <joni-makinen@live.fi>
2025-11-30 10:42:46 +0200

    Add question set titles

---
+++ frontend/src/config/questionSetTitles.js
@@ -0,0 +1,12 @@
+// Titles for each question set (0-8)
+export const QUESTION_SET_TITLES = [
+  "General Questions",
+  "Programming, Scripting, and Markup Languages Experience",
+  "Databases Experience",
+  "Cloud Development Experience",
+  "Web Frameworks and Technologies Experience",
+  "Dev IDEs Experience",
+  "Large Language Models Experience",
+  "Code Documentation and Collaboration Experience",
+  "Computer Operating Systems Experience",
+];
