39c52ec6de34634f00c72d2f4bfdb4ca57434030
Joni Mäkinen <joni-makinen@live.fi>
2025-12-01 18:06:02 +0200

    Import 'SearcherService' instead of 'SearcherAgent', and 'ScorerService' instead of 'ScorerAgent'

--- src/jobsai/agents/__init__.py
+++ src/jobsai/agents/__init__.py
@@ -1,6 +1,6 @@
 # Allows importing with: from agents import ...
 from .profiler import ProfilerAgent
-from .searcher import SearcherAgent
-from .scorer import ScorerAgent
+from .searcher import SearcherService
+from .scorer import ScorerService
 from .reporter import ReporterAgent
 from .generator import GeneratorAgent
