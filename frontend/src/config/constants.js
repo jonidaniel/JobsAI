03cdb2cf29b940ecd7c4be726cef1b8ecd49cd48
Joni Mäkinen <joni-makinen@live.fi>
2025-12-01 22:27:03 +0200

    Move  to

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -73,3 +73,34 @@ export const QUESTION_SET_NAMES = [
   "operating-systems",
   "additional-info",
 ];
+
+/**
+ * Question Set Titles
+ *
+ * Display titles for each of the 9 question sets.
+ * These titles are shown as headings above each question set.
+ *
+ * Index mapping:
+ * 0: General Questions
+ * 1: Programming, Scripting, and Markup Languages Experience
+ * 2: Databases Experience
+ * 3: Cloud Development Experience
+ * 4: Web Frameworks and Technologies Experience
+ * 5: Dev IDEs Experience
+ * 6: Large Language Models Experience
+ * 7: Code Documentation and Collaboration Experience
+ * 8: Computer Operating Systems Experience
+ * 9: Additional Information
+ */
+export const QUESTION_SET_TITLES = [
+  "General Questions",
+  "Programming, Scripting, and Markup Languages Experience",
+  "Databases Experience",
+  "Cloud Development Experience",
+  "Web Frameworks and Technologies Experience",
+  "Dev IDEs Experience",
+  "Large Language Models Experience",
+  "Code Documentation and Collaboration Experience",
+  "Computer Operating Systems Experience",
+  "Additional Information",
+];
