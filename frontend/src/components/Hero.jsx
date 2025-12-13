428daa530307ee6f0a795afb2b182e3584fc9413
Joni Mäkinen <joni-makinen@live.fi>
2025-12-13 06:53:11 +0200

    Update main page header

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -9,7 +9,7 @@ import "../styles/hero.css";
 export default function Hero() {
   return (
     <section id="hero">
-      <h1>asdJobsAI'ITs</h1>
+      <h1>JobsAI</h1>
       <p>Let us do your job searching for you</p>
     </section>
   );
