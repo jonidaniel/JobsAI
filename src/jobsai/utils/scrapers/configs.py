e1e3bbfb6637c92a09195fab919d394515794068
Joni Mäkinen <joni-makinen@live.fi>
2025-12-20 13:44:42 +0200

    Debug empty Indeed results

--- src/jobsai/utils/scrapers/configs.py
+++ src/jobsai/utils/scrapers/configs.py
@@ -132,9 +132,9 @@ INDEED_CONFIG = ScraperConfig(
     # job_card_selector="[data-jk]",
     # job_card_selector=".mainContentTable.css-131ju4w.eu4oa1w0",
     # job_card_selector="div[data-jk], .jobsearch-SerpJobCard[data-jk], [data-jk]",
-    job_card_selector=".cardOutline.tapItem.dd-privacy-allow.result.sponTapItem.desktop.hasSection.hasSection-default.nonRecommendation-section.vjs-highlight.eu4oa1w0",
+    # job_card_selector=".cardOutline.tapItem.dd-privacy-allow.result.sponTapItem.desktop.hasSection.hasSection-default.nonRecommendation-section.vjs-highlight.eu4oa1w0",
     # Fallback options (commented out, uncomment if data-jk doesn't work):
-    # job_card_selector=".job_seen_beacon",
+    job_card_selector=".job_seen_beacon",
     # job_card_selector=".resultContent.css-1o6lhys.eu4oa1w0",
     # job_card_selector=".css-1ac2h1w.eu4oa1w0",
     # job_card_selector=".resultContent",
