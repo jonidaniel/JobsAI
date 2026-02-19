a032bfc5295b4f8473b5683ceccee22e2d58045d
Joni Mäkinen <joni-makinen@live.fi>
2026-02-19 10:29:48 +0200

    Change header

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -9,7 +9,7 @@ import "../styles/hero.css";
 export default function Hero() {
   return (
     <section id="hero">
-      <h1>JobsAI</h1>
+      <h1>JobsAIIT</h1>
       <p>Let us do your job searching for you</p>
     </section>
   );
